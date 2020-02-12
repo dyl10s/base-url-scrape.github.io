c10ec3067c931907cff174e4093e871b6352239d
Dylan Strohschein <dylanstrohschein@gmail.com>
2020-02-12 01:43:04 -0500

    Finished base code

---
+++ BaseUrlFinder/Extensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BaseUrlFinder
+{
+    public static class Extensions
+    {
+        public static string[] splitString(this string input, string splitText)
+        {
+            return input.Split(new string[] { splitText }, StringSplitOptions.None);
+        }
+    }
+}
